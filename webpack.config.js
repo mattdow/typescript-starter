1ecd3e851a4aee2b7e5ceb311386c9dee4f9b986
Matt Dow <matt.dow.vo@gmail.com>
2022-01-25 12:35:13 -0600

    Adding TS to project, Hello World still functioning

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: 'development',
     context: path.join(__dirname, 'app'),
     entry: [
-        './js/app.js',
+        './js/app.ts',
     ],
     output: {
         path: path.join(__dirname, 'public'),
@@ -14,12 +14,10 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader',
-                ],
-            },
+              test: /\.ts$/,
+              use: 'ts-loader',
+              exclude: /node_modules/,
+            }
         ],
     },
     plugins: [new HtmlWebpackPlugin({
